fe390c7d13d3e0af324b594251914a74a15e501f
guicgs <etohlif@protonmail.com>
2020-09-11 21:45:47 -0300

    CC

--- tech_news_data_collector/news_scrapper.py
+++ tech_news_data_collector/news_scrapper.py
@@ -15,9 +15,18 @@ def fetch_content(url, timeout=2):
         return response.text
 
 
-def get_title(selector):
-    title = selector.css("h1#js-article-title::text").get()
-    return title and title.strip()
+def get_title_or_writer(selector, field):
+    result = ""
+    if field == "title":
+        result = selector.css("h1#js-article-title::text").get()
+    if field == "writer":
+        result = selector.css(".tec--author__info__link::text").get()
+    return result and result.strip()
+
+
+# def get_title(selector):
+#     title = selector.css("h1#js-article-title::text").get()
+#     return title and title.strip()
 
 
 def get_timestamp(selector):
@@ -27,9 +36,9 @@ def get_timestamp(selector):
     return timestamp
 
 
-def get_writer(selector):
-    writer = selector.css(".tec--author__info__link::text").get()
-    return writer and writer.strip()
+# def get_writer(selector):
+#     writer = selector.css(".tec--author__info__link::text").get()
+#     return writer and writer.strip()
 
 
 def get_shares_count(selector):
@@ -53,33 +62,46 @@ def get_summary(selector):
     return re.sub("\\n|\\xa0", "", summary)
 
 
-def get_sources(selector):
-    sources = selector.css(".z--mb-16 > div > a::text").getall()
-    sources_list = []
-    for source in sources:
-        sources_list.append(source.strip())
-    return ", ".join(sources_list)
+def get_categories_or_sources(selector, field):
+    response = []
+    if field == "sources":
+        sources = selector.css(".z--mb-16 > div > a::text").getall()
+        for source in sources:
+            response.append(source.strip())
+    if field == "categories":
+        categories = selector.css("#js-categories > a::text").getall()
+        for category in categories:
+            response.append(category.strip())
+    return ", ".join(response)
 
 
-def get_categories(selector):
-    categories = selector.css("#js-categories > a::text").getall()
-    categories_list = []
-    for category in categories:
-        categories_list.append(category.strip())
-    return ", ".join(categories_list)
+# def get_sources(selector):
+#     sources = selector.css(".z--mb-16 > div > a::text").getall()
+#     sources_list = []
+#     for source in sources:
+#         sources_list.append(source.strip())
+#     return ", ".join(sources_list)
+
+
+# def get_categories(selector):
+#     categories = selector.css("#js-categories > a::text").getall()
+#     categories_list = []
+#     for category in categories:
+#         categories_list.append(category.strip())
+#     return ", ".join(categories_list)
 
 
 def extract_news(url, selector):
     news_info = dict()
     news_info["url"] = url
-    news_info["title"] = get_title(selector)
+    news_info["title"] = get_title_or_writer(selector, "title")
     news_info["timestamp"] = get_timestamp(selector)
-    news_info["writer"] = get_writer(selector)
+    news_info["writer"] = get_title_or_writer(selector, "writer")
     news_info["shares_count"] = get_shares_count(selector)
     news_info["comments_count"] = get_comments_count(selector)
     news_info["summary"] = get_summary(selector)
-    news_info["sources"] = get_sources(selector).strip()
-    news_info["categories"] = get_categories(selector)
+    news_info["sources"] = get_categories_or_sources(selector, "sources")
+    news_info["categories"] = get_categories_or_sources(selector, "categories")
     return news_info
 
 
