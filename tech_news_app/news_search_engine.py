3baf21b0018ec592bd7943880d561adad2660c57
phtf92 <phtf92@gmail.com>
2020-09-14 20:16:24 -0300

    Req 7

--- tech_news_app/news_search_engine.py
+++ tech_news_app/news_search_engine.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import re
 
 
 def get_from_db(filter_query={}, projection={}):
@@ -16,8 +17,17 @@ def search_by_title(regex):
         print([])
 
 
-def search_by_date():
-    raise NotImplementedError
+def search_by_date(date):
+    date_pattern = re.compile("[0-9]{4}-[0-9]{2}-[0-9]{2}")
+    if not bool(date_pattern.match(date)):
+        print("Data inválida")
+    else:
+        regex_express = {"datetime": {'$regex': '^' + date, "$options": 'i'}}
+        news_list = list(get_from_db(regex_express, {"title": 1, "url": 1}))
+        for item in news_list:
+            print(f"- {item['title']} : {item['url']}")
+        if not len(news_list):
+            print([])
 
 
 def search_by_source():
@@ -28,4 +38,5 @@ def search_by_category():
     raise NotImplementedError
 
 
-search_by_title('paraxzcxc criar')
+# search_by_title('paraxzcxc criar')
+# search_by_date('2020-09-12')
