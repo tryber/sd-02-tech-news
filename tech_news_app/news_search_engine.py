184f8514598288435d56f7df68623eb425edf3a3
phtf92 <phtf92@gmail.com>
2020-09-14 19:00:03 -0300

    req 6

--- tech_news_app/news_search_engine.py
+++ tech_news_app/news_search_engine.py
@@ -1,5 +1,19 @@
-def search_by_title():
-    raise NotImplementedError
+from pymongo import MongoClient
+
+
+def get_from_db(filter_query={}, projection={}):
+    with MongoClient() as client:
+        db = client['web_scrape_python']
+        return db['news_collection'].find(filter_query, projection)
+
+
+def search_by_title(regex):
+    regex_expression = { "title": { '$regex': regex, "$options" :'i'}}
+    news_list = list(get_from_db(regex_expression, {"title": 1, "url": 1}))
+    for item in news_list:
+        print(f"- {item['title']} : {item['url']}")
+    if not len(news_list):
+        print([])
 
 
 def search_by_date():
@@ -12,3 +26,5 @@ def search_by_source():
 
 def search_by_category():
     raise NotImplementedError
+
+search_by_title('paraxzcxc criar')
