c2be3c020241737dcfd4815996bdc8d15d944821
phtf92 <phtf92@gmail.com>
2020-09-15 18:40:35 -0300

    CC

--- tech_news_data_collector/news_importer.py
+++ tech_news_data_collector/news_importer.py
@@ -16,14 +16,13 @@ def create_dict(keys, values):
 def save_db(item, db, collection, is_dict):
     with MongoClient() as client:
         db = client[db]
-        item_to_insert = item if not is_dict else create_dict(
-                                                        correct_header, item)
-        db[collection].insert_one(item_to_insert)
+        add_item = item if not is_dict else create_dict(correct_header, item)
+        db[collection].insert_one(add_item)
 
 
 def check_if_exists_db(value, i, collection, field):
     with MongoClient() as client:
-        db = client['web_scrape_python']
+        db = client["web_scrape_python"]
         new = list(db[collection].find({field: value}))
         if len(new) > 0:
             raise ValueError(f"Notícia {i} duplicada")
@@ -31,14 +30,14 @@ def check_if_exists_db(value, i, collection, field):
 
 def iterate_lines(csvLines):
     for line in csvLines:
-        save_db(line, 'web_scrape_python', 'news_collection', True)
+        save_db(line, "web_scrape_python", "news_collection", True)
 
 
 def check_errors_or_if_exists(csvLines, header):
     for index, line in enumerate(csvLines, start=1):
         line = [item for item in line if item]
-        check_comparison(len(line), len(header), f'Erro na notícia {index}')
-        check_if_exists_db(line[0], index, 'news_collection', 'url')
+        check_comparison(len(line), len(header), f"Erro na notícia {index}")
+        check_if_exists_db(line[0], index, "news_collection", "url")
 
 
 def check_header(headers, index):
@@ -50,18 +49,18 @@ def check_header(headers, index):
 def check_all_headers(news):
     for index, new in enumerate(news, start=1):
         check_header(list(new.keys()), index)
-        check_if_exists_db(new['url'], index, 'news_collection', 'url')
+        check_if_exists_db(new["url"], index, "news_collection", "url")
 
 
 def csv_importer(arg):
     try:
         extension = os.path.splitext(arg)[1]
-        check_comparison(extension, '.csv', 'Formato inválido')
+        check_comparison(extension, ".csv", "Formato inválido")
 
         with open(arg) as file:
             header, *csvLines = csv.reader(file, delimiter=";")
 
-            check_comparison(header, correct_header, 'Cabeçalho inválido')
+            check_comparison(header, correct_header, "Cabeçalho inválido")
 
             check_errors_or_if_exists(csvLines, header)
             iterate_lines(csvLines)
@@ -73,24 +72,28 @@ def csv_importer(arg):
         print(exc_ext, file=sys.stderr)
 
     else:
-        print('Importação realizada com sucesso', file=sys.stdout)
+        print("Importação realizada com sucesso", file=sys.stdout)
 
 
 def save_lines(news):
     for new in news:
-        save_db(new, 'web_scrape_python', 'news_collection', False)
+        save_db(new, "web_scrape_python", "news_collection", False)
+
+
+def open_file(arg):
+    with open(arg) as file:
+        return json.load(file)
 
 
 def json_importer(arg):
     try:
         extension = os.path.splitext(arg)[1]
-        check_comparison(extension, '.json', 'Formato inválido')
+        check_comparison(extension, ".json", "Formato inválido")
 
-        with open(arg) as file:
-            news = json.load(file)
+        news = open_file(arg)
 
-            check_all_headers(news)
-            save_lines(news)
+        check_all_headers(news)
+        save_lines(news)
 
     except FileNotFoundError:
         print(f"Arquivo {arg} não encontrado", file=sys.stderr)
@@ -102,9 +105,4 @@ def json_importer(arg):
         print(exc_ext, file=sys.stderr)
 
     else:
-        print('Importação realizada com sucesso', file=sys.stdout)
-
-
-# csv_importer('tech_news_data_collector/news.csv')
-
-# json_importer('tech_news_data_collector/news.json')
+        print("Importação realizada com sucesso", file=sys.stdout)
