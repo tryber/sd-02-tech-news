b5a45679759d3e020e92d909fb0ccf7bcabcd8b0
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-09-15 17:28:12 -0300

    Requisito 2

--- tech_news_data_collector/news_importer.py
+++ tech_news_data_collector/news_importer.py
@@ -1,6 +1,72 @@
-def csv_importer():
-    raise NotImplementedError
+import csv
+from os import path
+from pymongo import MongoClient
+
+def_headers = [
+    "url",
+    "title",
+    "timestamp",
+    "writer",
+    "shares_count",
+    "comments_count",
+    "summary",
+    "sources",
+    "categories",
+]
+
+data_to_insert = []
+
+
+def mongo_insert_if_not_found(data_to_insert):
+    with MongoClient() as client:
+        db = client["tech_news"]
+        collection = db["news_raspadas"]
+        for index, data in enumerate(data_to_insert):
+            item_exists = bool(collection.find_one({"url": data[0]}))
+            if not item_exists:
+                print(index, data)
+                collection.insert_one({
+                    "url": data[0],
+                    "title": data[1],
+                    "timestamp": data[2],
+                    "writer": data[3],
+                    "shares_count": data[4],
+                    "comments_count": data[5],
+                    "summary": data[6],
+                    "sources": data[7],
+                    "categories": data[8],
+                })
+
+
+def csv_importer(csv_file):
+    if not path.exists(csv_file):
+        print(f"Arquivo {csv_file} não encontrado")
+        return
+    if not csv_file.endswith(".csv"):
+        print("Formato inválido")
+        return
+
+    with open(csv_file) as file:
+        csv_info = csv.reader(file, delimiter=";")
+        header, *all_data = csv_info
+        if not header == def_headers:
+            print("Cabeçalho inválido")
+
+        for index, data in enumerate(all_data):
+            if not len(data) == 9:
+                print(f"Erro na notícia {index}")
+            for column in data:
+                if column == '':
+                    print(f"Erro na notícia {index}")
+                    return
+            data_to_insert.append(data)
+
+    mongo_insert_if_not_found(data_to_insert)
+    print("Importação realizada com sucesso")
 
 
 def json_importer():
     raise NotImplementedError
+
+
+csv_importer("sucesso.csv")
