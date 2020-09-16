7d117464d4464fc8dbc8d24a4f5f8e09d505d408
lauro <laurolyra@gmail.com>
2020-09-16 17:21:17 -0300

    0,5 req done

---
+++ exercício.py
@@ -0,0 +1,8 @@
+import json  # json é um modulo que vem embutido, porém precisamos importá-lo
+
+
+with open("pokemons.json") as file:
+    pokemons = json.load(file)["results"]  # o conteúdo é transformado em estrutura python equivalente, dicionário neste caso.
+    # acessamos a chave results que é onde contém nossa lista de pokemons
+
+print(pokemons[0])  # imprime o primeiro pokemon da lista
