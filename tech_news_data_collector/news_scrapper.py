83604ee4450f03fc5a1f2a1fb10fca728e5f783c
miguel-miranda <ayandram7727@gmail.com>
2020-09-20 21:11:04 -0300

    noticias estao sendo salvas no banco de dados conforme requisito 1

--- tech_news_data_collector/news_scrapper.py
+++ tech_news_data_collector/news_scrapper.py
@@ -1,20 +1,34 @@
 from parsel import Selector
+from pymongo import MongoClient
 import requests
 import time
 
 
+def fetch_content(url, timeout=10):
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+        time.sleep(6)
+    except requests.HTTPError:
+        return ""
+    else:
+        return response.text
+
+
 def scrape(n=1):
+    client = MongoClient()
+    db = client.tech_news
+
     URL_BASE = "https://www.tecmundo.com.br/novidades"
     # Define a primeira página como próxima a ter seu conteúdo recuperado
     next_page_url = URL_BASE
-    print(n)
 
     for i in range(n):
         print('olaa', i)
         # Busca o conteúdo da próxima página
-        response = requests.get(next_page_url, timeout=10)
-        time.sleep(6)
-        selector = Selector(text=response.text)
+        next_page_text = fetch_content(next_page_url)
+        selector = Selector(text=next_page_text)
+
         # Imprime as notícias de uma determinada página
         for news in selector.css(".tec--list__item"):
             # Busca e extrai o título e timestamp?????????????????????????????????????????
@@ -26,24 +40,23 @@ def scrape(n=1):
             detail_page_url = news.css(".tec--card__title__link::attr(href)").get()
             print(detail_page_url)
             outra_url = "https://www.tecmundo.com.br/dispositivos-moveis/177596-surface-duo-ganha-novo-comercial-focado-dobradica.htm"
-
+            outra = "https://www.tecmundo.com.br/internet/198331-youtube-music-libera-recurso-letras-musica-versao-web.htm"
             # Baixa o conteúdo da página de detalhes
-            detail_response = requests.get(outra_url, timeout=10)
-            time.sleep(6)
-            detail_selector = Selector(text=detail_response.text)
+            url = outra
+            detail_page_text = fetch_content(url)
+            detail_selector = Selector(text=detail_page_text)
+            print(detail_selector)
 
             # Extrai detalhes do produto
             timestamp = detail_selector.css("time::attr(datetime)").get()
             print(timestamp)
-            writer = detail_selector.css(".tec--author__info__link::text").get()
+            writer = detail_selector.css(".tec--author__info__link::text").re_first("[A-zÀ-ÿ][A-zÀ-ÿ' ]+")
             print(writer)
-            shares_count = detail_selector.css(".tec--toolbar__item::text").re(
+            shares_count = int(detail_selector.css(".tec--toolbar__item::text").re(
                 "[0-9]+"
-            )[0]
+            )[0])
             print(shares_count)
-            comments_count = detail_selector.css("#js-comments-btn::text").re("[0-9]+")[
-                0
-            ]
+            comments_count = int(detail_selector.css("#js-comments-btn::text").re("[0-9]+")[0])
             print(comments_count)
 
             # summary, sources, categories??W??????w?w???w????d????w?wwwwwwwwwwwwwwwwwwwwwwwwwww????wW??????wwW??????wwwww
@@ -62,12 +75,39 @@ def scrape(n=1):
             # description = description[: -len(suffix)]
             # print(description)
 
+            document = {
+                "url": url,
+                "title": title,
+                "timestamp": timestamp,
+                "writer": writer,
+                "shares_count": shares_count,
+                "comments_count": comments_count,
+                "summary": summary,
+                "sources": sources,
+                "categories": categories
+            }
+            document_exists = db.news.find_one({"url": url})
+            print(document_exists)
+            if document_exists:
+                print('entrou')
+                db.news.update_one(
+                    {"url": url},
+                    {"$set": document}
+                )
+            else:
+                db.news.insert_one(document)
+
         # Descobre qual é a próxima página
         print('antes', next_page_url)
         next_page_url = selector.css(
             ".tec--list--lg .tec--btn--primary::attr(href)"
         ).get()
         print('depois', next_page_url)
+
+    client.close()
+
+    #importanteeee
+    print("Raspagem de notícias finalizada")
 
 
 # print('sem parametro')
@@ -77,4 +117,4 @@ def scrape(n=1):
 # print('parametro 2')
 # scrape(2)
 print('parametro 3')
-scrape(3)
+scrape()
