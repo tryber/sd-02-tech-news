38952dcc031efe734f2b515cb524a661e4877f4a
richardwellerson <richardwellerson@gmail.com>
2020-09-27 16:44:25 -0300

    Trabalhando CSV

--- tech_news_data_collector/news_importer.py
+++ tech_news_data_collector/news_importer.py
@@ -1,6 +1,18 @@
-def csv_importer():
-    raise NotImplementedError
+import sys
+import csv
+
+
+def csv_importer(path):
+    with open(path) as csv_data:
+        csv_list = []
+        csv_news = csv.DictReader(csv_data, delimiter=";", quotechar='"')
+        for row in csv_news:
+            csv_list.append(dict(row))
+        print(csv_list)
 
 
 def json_importer():
     raise NotImplementedError
+
+
+csv_importer(sys.argv[1])
