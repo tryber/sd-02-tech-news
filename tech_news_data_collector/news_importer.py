4e2b4751b7967398bb9d3d7de1b57304075510c9
richardwellerson <richardwellerson@gmail.com>
2020-09-28 07:20:43 -0300

    Requisito 2, tratando caso final arquivo nao seja .CSV

--- tech_news_data_collector/news_importer.py
+++ tech_news_data_collector/news_importer.py
@@ -1,14 +1,18 @@
 import sys
 import csv
 
+need_keys = ["url", "title", "timestamp", "writer", "shares_count", "comments_count", "summary", "sources", "categories"]
+
 
 def csv_importer(path):
+    csv_list = []
+    if not path.endswith('.csv'):
+        return print('Formato inválido')
+
     with open(path) as csv_data:
-        csv_list = []
         csv_news = csv.DictReader(csv_data, delimiter=";", quotechar='"')
         for row in csv_news:
             csv_list.append(dict(row))
-        print(csv_list)
 
 
 def json_importer():
