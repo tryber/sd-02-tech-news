fe7e31a636775bd2d855b4a6b760b4d92186d89c
richardwellerson <richardwellerson@gmail.com>
2020-09-28 07:40:02 -0300

    Tratando a chave

--- tech_news_data_collector/news_importer.py
+++ tech_news_data_collector/news_importer.py
@@ -4,6 +4,11 @@ import csv
 need_keys = ["url", "title", "timestamp", "writer", "shares_count", "comments_count", "summary", "sources", "categories"]
 
 
+def verify_keys(all_dict, key):
+    if f"{key}" not in all_dict:
+        return print(f'Chave \"{key}\" não presente no arquivo.')
+
+
 def csv_importer(path):
     csv_list = []
     if not path.endswith('.csv'):
@@ -13,6 +18,8 @@ def csv_importer(path):
         csv_news = csv.DictReader(csv_data, delimiter=";", quotechar='"')
         for row in csv_news:
             csv_list.append(dict(row))
+    for key in need_keys:
+        verify_keys(csv_list, key)
 
 
 def json_importer():
