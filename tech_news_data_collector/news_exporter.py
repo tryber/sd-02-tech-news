a132608d9cd077e3ef788f9900e65a6a36668f4f
miguel-miranda <ayandram7727@gmail.com>
2020-09-29 21:26:53 -0300

    requisito 3 concluido

--- tech_news_data_collector/news_exporter.py
+++ tech_news_data_collector/news_exporter.py
@@ -1,5 +1,41 @@
-def csv_exporter():
-    raise NotImplementedError
+import csv
+import sys
+from pymongo import MongoClient
+
+
+def csv_exporter(file):
+    if not file.endswith(".csv"):
+        return print("Formato inválido", file=sys.stderr)
+
+    with open(file, "w") as csv_file:
+        header = [
+            "url",
+            "title",
+            "timestamp",
+            "writer",
+            "shares_count",
+            "comments_count",
+            "summary",
+            "sources",
+            "categories",
+        ]
+        writer = csv.DictWriter(csv_file, delimiter=";", fieldnames=header)
+        writer.writeheader()
+
+        client = MongoClient()
+        documents = client.tech_news.news.find({}, {"_id": 0})
+
+        for document in documents:
+            row = {}
+            for name, value in document.items():
+                if name == "sources" or name == "categories":
+                    row[name] = ",".join(value)
+                else:
+                    row[name] = value
+            writer.writerow(row)
+
+        print("Exportação realizada com sucesso", file=sys.stdout)
+        client.close()
 
 
 def json_exporter():
