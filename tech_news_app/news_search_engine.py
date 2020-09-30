ae32a7033271f2247dc6708073b7f712165f73ab
miguel-miranda <ayandram7727@gmail.com>
2020-09-30 16:11:33 -0300

    requisito 6 parece concluido

--- tech_news_app/news_search_engine.py
+++ tech_news_app/news_search_engine.py
@@ -1,5 +1,19 @@
-def search_by_title():
-    raise NotImplementedError
+from pymongo import MongoClient
+
+
+def search_by_title(search):
+    client = MongoClient()
+    db = client.tech_news
+
+    searched_news = list(db.news.find(
+        {"title": {"$regex": search, "$options": 'i'}},
+        {"title": 1, "url": 1, "_id": 0}
+    ))
+
+    return [
+        f"- {news['title']}: {news['url']}"
+        for news in searched_news
+    ]
 
 
 def search_by_date():
