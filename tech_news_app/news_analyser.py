9a5d31cf6bd0cddd41ed1b72839bf9254aa08b39
miguel-miranda <ayandram7727@gmail.com>
2020-09-30 19:31:38 -0300

    requisito 10 concluido

--- tech_news_app/news_analyser.py
+++ tech_news_app/news_analyser.py
@@ -1,5 +1,27 @@
+from pymongo import MongoClient
+
+
 def top_5_news():
-    raise NotImplementedError
+    with MongoClient() as client:
+        db = client.tech_news
+
+        searched_news = list(db.news.aggregate([
+            {
+                "$addFields": {
+                    "shares_and_comments": {
+                        "$sum": ["$shares_count", "$comments_count"]
+                    }
+                }
+            },
+            {"$sort": {"shares_and_comments": -1}},
+            {"$limit": 5},
+            {"$project": {"title": 1, "url": 1, "_id": 0}}
+        ]))
+
+        return [
+            f"- {news['title']}: {news['url']}"
+            for news in searched_news
+        ]
 
 
 def top_5_categories():
