27390e92fe73b0be4c253117a1e52a656c0f0042
lauro <laurolyra@gmail.com>
2020-10-02 18:35:47 -0300

    page scrapping ok. Aim for the loops

--- tech_news_data_collector/news_scrapper.py
+++ tech_news_data_collector/news_scrapper.py
@@ -16,37 +16,37 @@ def fetch_content(url, timeout=1):
         return response.text
     
 def scrape(n = 1):
-    response = req_answer()
-    selector = Selector(response)
-    titles = selector.css("h3 .tec--card__title__link::attr(href)").getall()
-    for link in titles:
-        url = link
-        print(url)
-        single_new = fetch_content('http://127.0.0.1:5500/tech_news_data_collector/Surface%20Duo%20ganha%20novo%20comercial%20focado%20na%20dobradi%C3%A7a%20-%20TecMundo.html')
-        # single_new = requests.get('http://127.0.0.1:5500/tech_news_data_collector/Surface%20Duo%20ganha%20novo%20comercial%20focado%20na%20dobradi%C3%A7a%20-%20TecMundo.html')
-        # selector = Selector(single_new.text)
-        title = selector.css(".tec--article__header__title::text").get()
-        print(title)
-        # print(title.strip()) # isso aqui pega o título
-        timestamp = selector.css("time::attr(datetime)").get()
-        print(timestamp)
-        writer = selector.css(".tec--author__info__link::text").get()
-        # print(writer.strip())
-        shares_count = selector.css(".tec--toolbar__item:first-child *::text").re_first(r"\d") or 0
-        # print(int(shares_count))
-        comments_count = selector.css(".tec--toolbar__item:nth-child(2) > button *::text").re_first(r"\d") or 0
-        # print(int(comments_count))
-        summary = selector.css(".tec--article__body p:first-child *::text").getall()
-        # print(''.join(summary))
-        def strip_list(src):
-            sources_list = []
-            for source in src:
-                sources_list.append(source.strip())
-            return sources_list
-        sources = selector.css(".z--mb-16 .tec--badge::text").getall()
-        # print(strip_list(sources))
-        categories = selector.css("#js-categories > a::text").getall()
-        # print(strip_list(categories))
+    # response = req_answer()
+    # selector = Selector(response)
+    # titles = selector.css("h3 .tec--card__title__link::attr(href)").getall()
+    # for link in titles:
+    # url = link
+    # print(url)
+    single_new = fetch_content('http://127.0.0.1:5500/tech_news_data_collector/Surface%20Duo%20ganha%20novo%20comercial%20focado%20na%20dobradi%C3%A7a%20-%20TecMundo.html')
+    # single_new = requests.get('http://127.0.0.1:5500/tech_news_data_collector/Surface%20Duo%20ganha%20novo%20comercial%20focado%20na%20dobradi%C3%A7a%20-%20TecMundo.html')
+    selector = Selector(single_new)
+    title = selector.css(".tec--article__header__title::text").get()
+    print(title.strip())
+    # print(title.strip()) # isso aqui pega o título
+    timestamp = selector.css("time::attr(datetime)").get()
+    print(timestamp)
+    writer = selector.css(".tec--author__info__link::text").get()
+    print(writer.strip())
+    shares_count = selector.css(".tec--toolbar__item:first-child *::text").re_first(r"\d") or 0
+    print(int(shares_count))
+    comments_count = selector.css(".tec--toolbar__item:nth-child(2) > button *::text").re_first(r"\d") or 0
+    print(int(comments_count))
+    summary = selector.css(".tec--article__body p:first-child *::text").getall()
+    print(''.join(summary))
+    def strip_list(src):
+        sources_list = []
+        for source in src:
+            sources_list.append(source.strip())
+        return sources_list
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    print(strip_list(sources))
+    categories = selector.css("#js-categories > a::text").getall()
+    print(strip_list(categories))
     print("Raspagem de notícias finalizada")
 
 scrape()
