cfdcb63308e103b16486a9f6031c30a74c14b8eb
eddyeboy27 <lipe0503@gmail.com>
2020-10-08 06:00:23 -0300

    Finalizado requisito 6

--- tech_news_app/news_search_engine.py
+++ tech_news_app/news_search_engine.py
@@ -1,9 +1,31 @@
-def search_by_title():
-    raise NotImplementedError
+import sys
+from pymongo import MongoClient
+from datetime import date
 
 
-def search_by_date():
-    raise NotImplementedError
+def search_by_title(title):
+    with MongoClient() as connection:
+        new_format = []
+        db = connection["tech_news"]
+        collection = db["scrapped_news"]
+        answer = collection.aggregate([
+            {"$match": {"title": {"$regex": title, "$options": "i"}}},
+            {"$project": {"_id": 0, "title": "$title", "url": "$url"}}
+        ])
+        result = list(answer)
+        for item in result:
+            new_format.append(f"- {item['title']}: {item['url']}")
+    return new_format
+
+
+def search_by_date(date_user):
+    try:
+        isValid = date.fromisoformat('1999-08-10')
+    except ValueError:
+        print("Data inválida", file=sys.stderr)
+        return
+    else:
+        return isValid
 
 
 def search_by_source():
@@ -12,3 +34,7 @@ def search_by_source():
 
 def search_by_category():
     raise NotImplementedError
+
+
+# print(search_by_title("GOO"))
+print(search_by_date('1999-08-10'))
