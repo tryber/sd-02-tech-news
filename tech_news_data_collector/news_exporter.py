958090f2b331f062949d399e0fb191d4ac513d9e
lauro <laurolyra@gmail.com>
2020-10-12 02:48:39 -0300

    req 3 done

--- tech_news_data_collector/news_exporter.py
+++ tech_news_data_collector/news_exporter.py
@@ -1,6 +1,43 @@
-def csv_exporter():
-    raise NotImplementedError
+import csv
+import sys
+
+from mongo_connection import export_to_csv
+
+
+def csv_exporter(filename):
+    with open(filename, "w") as file:  # só confia
+        if not filename.endswith(".csv"):
+            print("Formato inválido", file=sys.stderr)
+            sys.exit(1)
+        # escreve o cabeçalho
+        headers = [
+            "url",
+            "title",
+            "timestamp",
+            "writer",
+            "shares_count",
+            "comments_count",
+            "summary",
+            "sources",
+            "categories"
+        ]
+        writer = csv.DictWriter(file, fieldnames=headers)
+        writer.writeheader()
+        array_from_db = export_to_csv()
+        # thx miguel-miguelito
+        for document in array_from_db:
+            row = {}
+            for name, value in document.items():
+                if name == "sources" or name == "categories":
+                    row[name] = ",".join(value)
+                else:
+                    row[name] = value
+            writer.writerow(row)
+        print("Exportação realizada com sucesso")
 
 
 def json_exporter():
     raise NotImplementedError
+
+
+csv_exporter('xablau_exp.csv')
