5215f155453dd247ffa35ec74bab1fcc8718f2f8
Bolivar <webdevelopmentbolivar@gmail.com>
2020-10-30 20:52:13 -0300

    code climate 01

--- app/news_search_engine.py
+++ app/news_search_engine.py
@@ -1,16 +1,13 @@
-from database.index import search_news
+
 import datetime
+
+from app.news_service import get_news_list
 
 
 def search_by_title(term):
     regex = r"\b" + term + r"\b"
     query = {"title": {"$regex": regex, "$options": 'i'}}
-    news = search_news(query)
-    news_list = []
-    for each in news:
-        new = str("- " + each["title"] + ": " + each["url"])
-        news_list.append(new)
-    return news_list
+    return get_news_list(query)
 
 
 def search_by_date(date):
@@ -20,30 +17,15 @@ def search_by_date(date):
         raise ValueError("Data inválida")
     else:
         query = {"timestamp": {"$regex": date, "$options": 'i'}}
-        news = search_news(query)
-        news_list = []
-        for each in news:
-            new = str("- " + each["title"] + ": " + each["url"])
-            news_list.append(new)
-        return news_list
+        return get_news_list(query)
 
 
 def search_by_source(source):
     regex = r"\b" + source + r"\b"
     query = {"sources": {"$regex": regex, "$options": 'i'}}
-    news = search_news(query)
-    news_list = []
-    for each in news:
-        new = str("- " + each["title"] + ": " + each["url"])
-        news_list.append(new)
-    return news_list
+    return get_news_list(query)
 
 
 def search_by_category(category):
     query = {"categories": category}
-    news = search_news(query)
-    news_list = []
-    for each in news:
-        new = str("- " + each["title"] + ": " + each["url"])
-        news_list.append(new)
-    return news_list
+    return get_news_list(query)
